176b6b0fa100e5598eebf7f068e8c8bec8f4d659
Frederick-S <mao_xiaodan@hotmail.com>
2021-12-24 21:18:26 +0800

    init

---
+++ src/test/kotlin/nhk/NhkApplicationTests.kt
@@ -0,0 +1,13 @@
+package nhk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NhkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
